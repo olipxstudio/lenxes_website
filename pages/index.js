7e0db3e6fe06e4e9e5f0fe6581eb135fbfdfaea5
olipxstudio <102828903+olipxstudio@users.noreply.github.com>
2022-07-12 20:59:15 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-          <p>Good</p>
+          <p>Good to go</p>
         </h1>
 
         <p className={styles.description}>
